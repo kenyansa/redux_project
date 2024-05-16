d2887e0ec5e29655b138942c5cb2fbc034db3194
kenyansa <kenyansajustine@gmail..com>
2024-05-16 14:22:16 +0300

    start a redux app by creating a reducer

--- redux.js
+++ redux.js
@@ -41,7 +41,33 @@ const asyncDataReducer = (state = defaultState, action) => {
   }
 };
 
+// const store = Redux.createStore(
+//   asyncDataReducer,
+//   Redux.applyMiddleware(ReduxThunk.default)
+// );
+
+// case 2: creating a redux app
+const INCREMENT = 'INCREMENT'; // Define a constant for increment action types
+const DECREMENT = 'DECREMENT'; // Define a constant for decrement action types
+
+const counterReducer = (state = 0, action) =>{
+    switch (action.type){
+        case INCREMENT:
+            return state + 1;
+        case DECREMENT:
+            return state - 1;
+        default:
+            return state
+    }
+}; // Define the counter reducer which will increment or decrement the state based on the action it receives
+
+const incAction = null; // Define an action creator for incrementing
+
+const decAction = null; // Define an action creator for decrementing
+
+
 const store = Redux.createStore(
-  asyncDataReducer,
-  Redux.applyMiddleware(ReduxThunk.default)
-);
+    asyncDataReducer,
+    Redux.applyMiddleware(ReduxThunk.default)
+  );
+// Define the Redux store here, passing in your reducers
